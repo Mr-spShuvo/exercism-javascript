40e902f9aa2b6e8bf28ed9e12f0403a0ddf2d6be
Md. Shahriar Parvez <mr.spshuvo@gmail.com>
2020-12-05 12:52:33 +0600

    :heavy_check_mark: pass space-age

--- exercises/space-age/space-age.js
+++ exercises/space-age/space-age.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, ageInSeconds) => {
+  const orbitalPeriods = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  }
+  const spaceAge = (ageInSeconds / 31557600) / orbitalPeriods[planet];
+  return Number(spaceAge.toFixed(2));
 };
