2c37b396169c98bc3a9b8d8e8c437fbf38ecea15
Md. Shahriar Parvez <mr.spshuvo@gmail.com>
2020-12-05 18:45:00 +0600

    :heavy_check_mark: pass pangram

--- exercises/pangram/pangram.js
+++ exercises/pangram/pangram.js
@@ -3,6 +3,41 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = sentence => {
+  const letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+
+  for (let letter of letters) {
+    if (!sentence.toLowerCase().includes(letter)) return false;
+  }
+  return true;
 };
